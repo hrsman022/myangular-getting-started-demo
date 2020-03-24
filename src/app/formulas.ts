03fe1821a697b8a43d2bbbb3c745c1d205f31425
hrsman022 <dillanclymer@gmail.com>
2020-03-24 15:56:19 -0400

    1 file modified, 1 added

---
+++ src/app/formulas.ts
@@ -0,0 +1,27 @@
+export const formulas = [
+  {
+    name: 'ARV',
+    description: 'After repair value',
+    value: 'AVG COMP Value'
+  },
+  {
+    name: 'Repair Cost - Tier 1',
+    description: 'Tier 1: Cosmetic Rehab',
+    value: ''
+  },
+    {
+    name: 'Repair Cost - Tier 2',
+    description: 'Tier 2: Tier 1 + Kitchen & Bathrooms',
+    value: ''
+  },
+    {
+    name: 'Repair Cost - Tier 3',
+    description: 'Tier 3: Tier 1 & 2 + a few big ticket items',
+    value: ''
+  },
+  {
+    name: 'MAO',
+    description: 'Maximum Allowed Offer',
+    value: 'ARV * .70 - RepairCostTier - AssignmentFee'
+  },
+]
