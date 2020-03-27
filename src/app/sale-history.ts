de2d0fdc082ee611f9cab3a996a835d176527e58
hrsman022 <dillanclymer@gmail.com>
2020-03-27 16:42:27 -0400

    1 file modified, 1 added

---
+++ src/app/sale-history.ts
@@ -0,0 +1,15 @@
+export const SaleHistory = [
+  {
+    address: 'Boufford Builders Inc',
+    purchaseprice: '55000',
+    purchasedate: '04-04-2019'
+    flippeddate: '02-24-2020'
+  },
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
