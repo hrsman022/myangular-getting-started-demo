e73f4c5665abc0ec790299dd56ead973824dab20
hrsman022 <dillanclymer@gmail.com>
2020-03-31 13:14:00 -0400

    2 files modified

--- src/app/clients.ts
+++ src/app/clients.ts
@@ -1,49 +1,38 @@
-export const clients = [
-  {
-    business: 'Boufford Builders Inc',
-    owner: 'James Boufford',
-    address: '8411 W Freeland Rd',
-    lat: '43.5191679',
-    long: '-84.0816417',
-    city: 'Freeland',
-    state: 'MI',
-    zip: '48623',
-    phone: '9896954663',
-    email: '',
-    fliplocation: 'Saginaw',
-    flipped: '1611 Van Wagoner Dr, Saginaw, MI 48638',
-    fliplong: '43.4313021',
-    fliplat: '-84.0251066',
-  },
-  {
-    business: 'KKR Properties LLC',
-    owner: 'Kevin Reed & Kelly Clarkson',
-    address: '3936 Shadbush',
+export const clients = 
+[{
+  business: 'Dallas Clymer Inc',
+  owner: 'Dallas Clymer',
+  address: '828 E. Seidlers Rd',
+  lat: '43.5191679',
+  long: '-84.0816417',
+  city: 'Kawkawlin',
+  state: 'MI',
+  zip: '48631',
+  phone: '9893240820',
+  email: 'dallasclymer@gmail.com',
+  fliplocation: 'Kawkawlin',
+  flipped: '828 E. Seidlers Rd, Kawkawlin, MI, MI 48631',
+  homes:[{
+    address: {
+    addressLine1: '1611 Van Wagoner Dr',
+    addressLine2: '',
     city: 'Saginaw',
     state: 'MI',
-    zip: '48603',
-    phone: '',
-    email: '',
-    fliplocation: 'Bay City',
-    flipped: '3181 Arnold CT, Bay City, MI 48706',
-  },
-  {
-    business: 'Biz Name',
-    owner: 'Jordan Pena',
-    address: 'Address',
-    city: 'City',
+    zip: 48638
+    },
+    lng: '43.4313021',
+    lat: '-84.0251066',
+    },
+    {
+    address: {
+    addressLine1: '828 E. Seidlers Rd',
+    addressLine2: '',
+    city: 'Kawkawlin',
     state: 'MI',
-    zip: 'Zip',
-    phone: 'Phone',
-    email: 'email@email.com',
-    fliplocation: 'Midland',
-    flipped: '5119 Drake St, Midland, MI 48640',
-  }
-];
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
+    zip: 48631
+    },
+    lng: '43.681797',
+    lat: '-84.015594',
+    },
+    ]
+},];
